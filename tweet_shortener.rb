53eb1b7549902204c6ef788ef7d91dba805f1e79
Ryan Namgung <heon.namgung@richmond.edu>
2018-06-19 06:17:20 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -33,4 +33,10 @@ def bulk_tweet_shortener(tweets)
   end
 end
 
-def selective_tweet_shortener
+def selective_tweet_shortener(tweet)
+  if tweet.length > 140 
+    word_substituter(tweet)
+  else
+    tweet
+  end
+end
