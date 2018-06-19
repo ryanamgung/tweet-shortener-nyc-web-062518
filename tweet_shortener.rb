550ed865d704a996a6be19b00162aa20bdf9b955
Ryan Namgung <heon.namgung@richmond.edu>
2018-06-19 05:54:27 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,4 +1,15 @@
 # Write your code here.
 def dictionary
-  {hello: "hi", to: "2", two: "2", too: "2", "for": "4", four: "4", be: "b", you: "u", at: "@", "and": "&"}
+ {
++  "hello" => "hi",
++  "to" => "2",
++  "two" => "2",
++  "too" => "2",
++  "for" => "4",
++  "four" => "4",
++  "be" => "b",
++  "you" => "u",
++  "at" => "@",
++  "and" => "&"
++  }
 end
